38d02dbecee88017503ecffe1776aa0e88364f0c
AswinCG <cgaswin333@gmail.com>
2024-04-13 18:44:25 +0530

    feat: initial project setup: setup server and connected to database

---
+++ server/src/app.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import express, { type Application } from "express";
+import cors, { type CorsOptions } from "cors";
+import helmet from "helmet";
+import rateLimit, { type RateLimitRequestHandler } from "express-rate-limit";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	} as CorsOptions)
+);
+
+app.use(helmet());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+const limiter: RateLimitRequestHandler = rateLimit({
+	windowMs: 15 * 60 * 1000, //15 mins
+	limit: 100,
+	legacyHeaders: false,
+	message: "Too many requests from this ip, please try after 15 mins",
+});
+
+app.use("/api", limiter);
+
+
+export default app;
---
+++ server/src/config/db.ts
@@ -0,0 +1,17 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const DB_URI: string = process.env.DB_URI || "";
+
+const connectWithDB = async (): Promise<Mongoose> => {
+	try {
+		const conn = await mongoose.connect(DB_URI);
+		console.log(`Connection to DB successful: ${conn.connection.host} `);
+		return mongoose;
+	} catch (error) {
+		console.error("DB connection failed");
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+export default connectWithDB;
